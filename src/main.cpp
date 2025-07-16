92b72012fbb2130ad0280529a56352276508f4bc
Ben <benxian456@gmail.com>
2025-07-16 20:08:34 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <thread>
 #include <chrono>
-#include "tinyLogger.h"
+#include "logger/tinyLogger.h"
 #include <signal.h>
-#include "hazardPointerGuard.h"
+#include "logger/hazardPointerGuard.h"
 
 volatile bool running = true;
 
@@ -51,9 +51,42 @@ void test_multi_thread_logger(TinyLogger& logger)
 }
 
 
+struct PoorlyAlignedData
+{
+    char     c;
+    uint16_t u;
+    double   d;
+    int16_t  i;
+};
+
+struct WellAlignedData
+{
+    double   d;
+    uint16_t u;
+    int16_t  i;
+    char     c;
+};
+
+#pragma pack(push, 1)
+struct PackedData
+{
+    double   d;
+    uint16_t u;
+    int16_t  i;
+    char     c;
+};
+#pragma pack(pop)
+
+struct MyStruct
+{
+    int8_t a;
+    int8_t b;
+    int16_t c;
+};
+
+
 void test_multi_thread_hazard_pointer()
 {
-
     // Consumers: log periodically
     std::vector<std::jthread> consumers;
     for (int i = 0; i < 3; ++i)
@@ -84,6 +117,26 @@ void test_multi_thread_hazard_pointer()
 
 int main()
 {
+    printf("PoorlyAlignedData c:%lu u:%lu d:%lu i:%lu size:%lu\n",
+           offsetof(struct PoorlyAlignedData, c),
+           offsetof(struct PoorlyAlignedData, u),
+           offsetof(struct PoorlyAlignedData, d),
+           offsetof(struct PoorlyAlignedData, i),
+           sizeof(PoorlyAlignedData));
+    printf("WellAlignedData d:%lu u:%lu i:%lu c:%lu size:%lu\n",
+           offsetof(struct WellAlignedData, d),
+           offsetof(struct WellAlignedData, u),
+           offsetof(struct WellAlignedData, i),
+           offsetof(struct WellAlignedData, c),
+           sizeof(WellAlignedData));
+    printf("PackedData d:%lu u:%lu i:%lu c:%lu size:%lu\n",
+           offsetof(struct PackedData, d),
+           offsetof(struct PackedData, u),
+           offsetof(struct PackedData, i),
+           offsetof(struct PackedData, c),
+           sizeof(PackedData));
+
+
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
 
